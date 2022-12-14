cbb4b65068f2a1b5ff07e807639574cae66747b8
Timur-Kotelnikov <104839071+Timur-Kotelnikov@users.noreply.github.com>
2022-12-14 21:48:44 +0300

    Update settings.py

--- hw_django/hw_2/netology_2/netology_2/settings.py
+++ hw_django/hw_2/netology_2/netology_2/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'phone_db',
         'USER': 'postgres',
-        'PASSWORD': '74576',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }
