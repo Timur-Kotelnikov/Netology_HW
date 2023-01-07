ef3b2d8a53857961fa36be1954006189e610603a
Timur-Kotelnikov <crowly086@gmail.com>
2023-01-07 10:18:34 +0300

    hotfix

--- hw_django/hw_5/hw_drf/hw_drf/settings.py
+++ hw_django/hw_5/hw_drf/hw_drf/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hw_drf',
         'USER': 'postgres',
-        'PASSWORD': '74576',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432'
     }
