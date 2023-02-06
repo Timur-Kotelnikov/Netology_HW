aa101596c030f47c16f4172beb87a8e87d610db2
Timur-Kotelnikov <crowly086@gmail.com>
2023-02-06 20:48:26 +0300

    remade hw7

--- hw_django/hw_7/hw_permissions/serializers.py
+++ hw_django/hw_7/hw_permissions/serializers.py
@@ -14,6 +14,6 @@ class AdvSerializer(serializers.ModelSerializer):
         adv = Adv.objects.filter(user=self.context["request"].user, status=True)
         print(adv)
         print(len(adv))
-        if len(adv) > 7 and self.context['request'].method == 'POST':
+        if len(adv) > 9 and self.context['request'].method == 'POST':
             raise ValidationError('You dont have permissions to have 10 or more adv at the same time')
         return data
